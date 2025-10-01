9b366768dbce103957a873315ab8ebfd3d08e284
Supanida Saesim <s6806022510246@email.kmutnb.ac.th>
2025-10-01 18:02:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import create  # 
+import create  # ใช้ฟังก์ชันใน create.py
 
 def main():
     while True:
@@ -7,6 +7,12 @@ def main():
         print("2) Add Member")
         print("3) View Books")
         print("4) View Members")
+        print("5) Search Book by ID")
+        print("6) Search Member by ID")
+        print("7) Update Book")
+        print("8) Update Member")
+        print("9) Delete Book")
+        print("10) Delete Member")
         print("0) Exit")
         choice = input("Select option: ")
 
@@ -18,6 +24,18 @@ def main():
             create.view_books()
         elif choice == "4":
             create.view_members()
+        elif choice == "5":
+            create.search_book()
+        elif choice == "6":
+            create.search_member()
+        elif choice == "7":
+            create.update_book()
+        elif choice == "8":
+            create.update_member()
+        elif choice == "9":
+            create.delete_book()
+        elif choice == "10":
+            create.delete_member()
         elif choice == "0":
             print("Exiting program...")
             break
